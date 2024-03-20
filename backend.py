975aa13d8414ab3a8589f8315204e4b5ad9b7bcd
marianadiaz179 <m.diaza2@uniandes.edu.co>
2024-03-20 09:01:34 -0500

    API smart_feature done

--- backend.py
+++ backend.py
@@ -3,49 +3,63 @@ from firebase_admin import firestore, initialize_app, credentials
 
 app = Flask(__name__)
 
-# Initialize Firebase with project ID
 cred = credentials.Certificate('key.json')
 initialize_app(cred)
 
 
 def getRestaurants():
     restaurants = []
-    restaurant_docs = firestore.client().collection('restaurants').get()
-    for doc in restaurant_docs:
-        restaurants.append(doc.to_dict())
-    
+    try:
+        restaurant_docs = firestore.client().collection('restaurants').get()
+        
+        for doc in restaurant_docs:
+            restaurants.append(doc.to_dict())
+    except Exception as e:
+        print(f"Error al obtener la lista de restaurantes: {e}")
+        return None 
+
     return restaurants
 
-userId = "8GjEXLg6ZrdN1pW2OSPWA9CIpVG3"
 
 def getUserPreferences(user):
     prefs = []
-    user_prefs = firestore.client().collection('users').document(user).collection('preferences').get()
-    
-    for pref in user_prefs:
-        prefs.append(pref.to_dict())
+    try:
+        user_prefs = firestore.client().collection('users').document(user).collection('preferences').get()
+        
+        for pref in user_prefs:
+            prefs.append(pref.to_dict())
+    except Exception as e:
+        print(f"Error al obtener las preferencias del usuario: {e}")
+        return None 
 
     return prefs
 
 
-
-def get_recommend_restaurants_by_tastes():
+def get_recommend_restaurants_by_tastes(userId):
     
     user_preferences = getUserPreferences(userId)
     restaurants = getRestaurants()
     
     matching_restaurants = []
+
+    if not user_preferences or 'tastes' not in user_preferences[0] or not user_preferences[0]['tastes']:
+        return matching_restaurants
+    
     for restaurant in restaurants:
         if any(pref in restaurant['foodType'] for pref in user_preferences[0]['tastes']):            
-            matching_restaurants.append(restaurant)  # Assuming the restaurant document has a 'name' field
+            matching_restaurants.append(restaurant) 
     
     return matching_restaurants
 
-def get_recommend_restaurants_by_price(user_id):
-    user_preferences = getUserPreferences(user_id)
+def get_recommend_restaurants_by_price(userId):
+    user_preferences = getUserPreferences(userId)
     restaurants = getRestaurants()
     
     matching_restaurants = []
+    
+    if not user_preferences or 'priceRange' not in user_preferences[0] or not user_preferences[0]['priceRange']:
+        return matching_restaurants
+    
     for restaurant in restaurants:
 
         min_price = user_preferences[0]['priceRange']['minPrice']
@@ -59,27 +73,32 @@ def get_recommend_restaurants_by_price(user_id):
     return matching_restaurants
 
 
-def get_recommend_restaurants_by_restrictions():
-    
+def get_recommend_restaurants_by_restrictions(userId):
     user_preferences = getUserPreferences(userId)
     restaurants = getRestaurants()
     
     matching_restaurants = []
+
+    if not user_preferences or 'restrictions' not in user_preferences[0] or not user_preferences[0]['restrictions']:
+        return matching_restaurants
+    
     for restaurant in restaurants:
         if any(pref in restaurant['dietaryConditions'] for pref in user_preferences[0]['restrictions']):            
-            matching_restaurants.append(restaurant)  # Assuming the restaurant document has a 'name' field
+            matching_restaurants.append(restaurant) 
     
     return matching_restaurants
 
-@app.route('/recommend', methods=['GET'])
-def filter_recommend(categoryFilter):
-    
+
+@app.route('/recommend/<string:userId>/<string:categoryFilter>', methods=['GET'])
+def filter_recommend(userId, categoryFilter):
     if categoryFilter == 'tastes':
-        get_recommend_restaurants_by_tastes()
+        return get_recommend_restaurants_by_tastes(userId)
     elif categoryFilter == 'price':
-        get_recommend_restaurants_by_price()
+        return get_recommend_restaurants_by_price(userId)
     elif categoryFilter == 'restrictions':
-        get_recommend_restaurants_by_restrictions()
+        return get_recommend_restaurants_by_restrictions(userId)
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000)
